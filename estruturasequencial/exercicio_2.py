22ac200685223dc77498b41334a328f388f5a997
Henriquelima1984 <henriquecorreia100@gmail.com>
2021-09-16 18:00:26 -0300

    Concluído exercicio de número 2

---
+++ estruturasequencial/exercicio_2.py
@@ -0,0 +1,8 @@
+"""
+2. Faça um Programa que peça um número e então mostre a mensagem (O número informado foi [número]).
+
+Exercício simples porém agora iremos utilizar a função input!
+"""
+
+numero = float(input('Digite um número qualquer: '))
+print(f' O número informado foi {numero} ')
