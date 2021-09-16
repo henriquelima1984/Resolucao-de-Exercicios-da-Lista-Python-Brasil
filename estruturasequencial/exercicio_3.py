f736e7eba9f9e4126e1f9c9eae65e7b2ed4cc17a
Henriquelima1984 <henriquecorreia100@gmail.com>
2021-09-16 18:03:16 -0300

    Concluído exercicio de número 3

---
+++ estruturasequencial/exercicio_3.py
@@ -0,0 +1,18 @@
+"""
+
+3. Faça um Programa que peça dois números e imprima a soma.
+
+Você pode fazer assim
+
+>>> soma = num1 + num2
+>>> print(soma)
+
+"""
+
+num1 = float(input('Digite o primeiro número: '))
+num2 = float(input('Digite o segundo número: '))
+
+#Ou pode fazer a operação dessa forma eliminandoa variável soma
+num1 += num2
+
+print(num1)
