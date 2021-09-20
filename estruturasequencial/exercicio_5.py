32d4be3e8bd6129f5fa89b56e306d0009e3c4a3c
Henriquelima1984 <henriquecorreia100@gmail.com>
2021-09-20 18:28:07 -0300

    Exercício 5 resolvido!

---
+++ estruturasequencial/exercicio_5.py
@@ -0,0 +1,12 @@
+"""
+5. Faça um Programa que converta metros para centímetros.
+
+Ex: 1 metro = 100 cm
+    2 m = x cm ?
+"""
+
+valor_em_metro = float (input('Digite o valor em metro(s): '))
+
+metro_para_centimetro = valor_em_metro * 100
+
+print(f' {valor_em_metro} metro(s) = {metro_para_centimetro} centímetro(s) ')
