6f05ed31c9cd77eaa00479cab827d0fd4f6c5ef0
Henriquelima1984 <henriquecorreia100@gmail.com>
2021-09-23 20:36:04 -0300

    Exercício 11 resolvido

---
+++ estruturasequencial/exercicio_11.py
@@ -0,0 +1,16 @@
+# 11. Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+# o produto do dobro do primeiro com metade do segundo .
+# a soma do triplo do primeiro com o terceiro.
+# o terceiro elevado ao cubo.
+
+n1 = int(input('Digite n1: '))
+n2 = int(input('Digite n2: '))
+n3 = int(input('Digite n3: '))
+
+produto = (n1*2) * n2/2
+soma = (n1*3) + n3
+potenciacao = n3**3
+
+print(f'O produto do dobro do primeiro com a metade do segundo: {produto}')
+print(f'A soma do triplo do primeiro com o terceiro: {soma}')
+print(f'O terceiro elevado ao cubo: {potenciacao}')
