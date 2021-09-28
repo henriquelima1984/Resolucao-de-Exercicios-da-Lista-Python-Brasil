9b937041d2195ccd03a66e98c4358ba4678b0198
Henriquelima1984 <henriquecorreia100@gmail.com>
2021-09-28 12:38:00 -0300

    Exercício 16 resolvido!

---
+++ estruturasequencial/exercicio_16.py
@@ -0,0 +1,17 @@
+#16.Faça um programa para uma loja de tintas.
+#O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.
+#Considere que a cobertura da tinta é de 1 litro para cada 3 metros
+#quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00.
+#Informe ao usuário a quantidade de latas de tinta a serem compradas e o preço total.
+# math.ceil() -->  arredonda para o número inteiro de cima mais próximo.
+# math.floor() --> arredonda para o número inteiro de baixo mais próximo.
+
+import math
+
+area_a_ser_pintada = float(input('Informe a área em metros quadrados: '))
+litro_por_metro = 3
+litros_a_serem_usados = area_a_ser_pintada / litro_por_metro
+litro_por_lata = 18
+qtd_de_latas_a_serem_usadas = math.ceil(litros_a_serem_usados / litro_por_lata)
+preco_total = qtd_de_latas_a_serem_usadas * 80
+print(f'Você precisa comprar {qtd_de_latas_a_serem_usadas} lata(s) de tinta de 18 litros no valor de {preco_total} R$')
