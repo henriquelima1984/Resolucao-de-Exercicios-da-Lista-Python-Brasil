cd2bb710440f8d8316a0a1a08ec86741bbda42d5
Vinicius Braga <87455091+ViniciusBrag@users.noreply.github.com>
2021-12-02 22:55:45 -0300

    Update exercicio_16.py

--- estruturasequencial/exercicio_16.py
+++ estruturasequencial/exercicio_16.py
@@ -6,12 +6,14 @@
 # math.ceil() -->  arredonda para o número inteiro de cima mais próximo.
 # math.floor()-->  arredonda para o número inteiro de baixo mais próximo.
 
-import math
+from math import ceil
 
 area_a_ser_pintada = float(input('Informe a área em metros quadrados: '))
 litro_por_metro = 3
 litros_a_serem_usados = area_a_ser_pintada / litro_por_metro
 litro_por_lata = 18
-qtd_de_latas_a_serem_usadas = math.ceil(litros_a_serem_usados / litro_por_lata)
+qtd_de_latas_a_serem_usadas = ceil(litros_a_serem_usados / litro_por_lata)
 preco_total = qtd_de_latas_a_serem_usadas * 80
 print(f'Você precisa comprar {qtd_de_latas_a_serem_usadas} lata(s) de tinta de 18 litros no valor de {preco_total} R$')
+
+#melhorando formas de importações de bibliotecas do python
